b392b7b9f9684e11111b0dcd16abe599967e42a8
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-10-14 13:50:06 +0700

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,5 @@
+nama = input("masukkan nama: ")
+umur = int(input("masukkan umur: "))
+tinggi = float(input("masukkan tinggi: "))
+print('nama: ',nama,"\n", 'umur: ',umur,"\n", 'tinggi: ',tinggi)
+               
