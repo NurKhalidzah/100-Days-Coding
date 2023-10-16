c588fe6098771b2df45dc6cb9753214e0772e771
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-10-16 07:36:38 +0700

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,6 @@
+gaji_pokok = int(input("gaji pokok: "))
+lama_lembur = int(input("lama lembur: "))
+
+Gaji_lembur = lama_lembur * 55000 
+gaji_bersih = gaji_pokok+Gaji_lembur
+print("Total Gaji Bersih: ",gaji_bersih)
