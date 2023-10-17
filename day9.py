78382cf6ce7c5f81426234da21844bba03fef5d6
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-10-17 12:08:06 +0700

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,8 @@
+#ninth commit(if else)
+
+angka1 = int(input('masukkan angka 1: '))
+angka2 = int(input('masukkan angka 2: '))
+if angka1 > angka2:
+    print('Angka 1 lebih besar')
+else:
+    print('Angka 2 lebih besar')
