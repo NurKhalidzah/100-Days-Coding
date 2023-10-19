51ae6185ba6c56f9f42515321f36ce0ba887c345
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-10-19 16:28:11 +0700

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,6 @@
+#Soal 2
+penghasilan_bulanan = 13000000 
+tabungan_per_bulan = 5000000 *12
+harga_pc_gaming = 150000000
+total = harga_pc_gaming / tabungan_per_bulan
+print("Sambo perlu menabung selama",total,"tahun untuk membeli PC gaming.")
