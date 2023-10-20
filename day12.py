fcd66a986125440f61e14807d9ff6ee6436a7ec8
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-10-20 11:55:49 +0700

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,14 @@
+#if else
+a = int(input("masukkan nilai anda: "))
+if a >=80 and a <=100:
+    print("Nilai Anda A")
+elif a >=70 and a <=80:
+    print("Nilai anda B")
+elif a >=60 and a <=70:
+    print("Nilai anda C")
+elif a >=50 and a <=60:
+    print("Nilai anda D")
+elif a <=50:
+    print("Nilai anda ERROR (Semangat Mengulang)")
+else:
+    print("Nilai Yang Anda Masukkan Tidak Valid")
