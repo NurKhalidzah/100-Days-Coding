bd6adf2bc86325b79786ebe4757afd6052d6d9d6
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-10-22 12:32:21 +0700

    day14 if ilse

---
+++ day14.py
@@ -0,0 +1,7 @@
+username = input("masukkan username anda: ")
+password = int(input("masukkan password anda: "))
+
+if username == 'ica' and password == 123:
+    print("Username dan Password anda benar")
+else:
+    print("Silahkan Periksa Kembali Username atau Password anda")
