999044d345e61cd89ef33c8b1c0398772630b2c2
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-10-23 08:51:17 +0700

    day15 Aritmatika (luas, keliling persegi panjang)

---
+++ day15.py
@@ -0,0 +1,7 @@
+print("Menghitung Luas & Keliling Persegi Panjang")
+panjang = float(input("masukkan panjang: "))
+lebar = float(input("masukkan lebar: "))
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+print("Luas Persegi Panjang Adalah: ",luas)
+print("Luas Keliling Persegi panjang Adalah: ",keliling)
