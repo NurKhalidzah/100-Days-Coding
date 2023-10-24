55efbc5cecce8b176f17ff5bfdbe285e544278d7
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-10-24 07:05:45 +0700

    day16 aritmatika (hitung luas keliling segitiga)

---
+++ day16.py
@@ -0,0 +1,13 @@
+print("Menghitung Luas Segitiga")
+alas = float(input("Alas segitiga: "))
+tinggi = float(input("Tinggi segitiga: "))
+luas =0.5 * alas * tinggi
+print("Luas Segitiga ",luas)
+
+print("==============================")
+print("Menghitung Keliling segitiga")
+a = int(input("sisi 1: "))
+b = int(input("sisi 2: "))
+c = int(input("sisi 3: "))
+kel = a + b + c
+print("Keliling Segitiga: ",kel)
