591cbd6afe2ac5d5c8c478cce49ba2b0b3c90f05
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-11-06 11:17:16 +0800

    Day 29 List (mengakses item)

---
+++ day29.py
@@ -0,0 +1,9 @@
+hari = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'ahad']
+
+print(hari[0])
+print(hari[1])
+print(hari[2])
+print(hari[3])
+print(hari[4])
+print(hari[5])
+print(hari[-1])
