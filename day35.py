50a56ca8953e864e22349efa3dbbe5f16f3d157d
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-11-12 19:40:20 +0800

    Day 35. Mengurutkan item array

---
+++ day35.py
@@ -0,0 +1,8 @@
+#mengurutkan dari nilai terbesar
+nilai = [76,89,34,65,20,12,63]
+nilai.sort(reverse=True)
+print(nilai)
+print("=============================")
+#mengurutkan dari nilai terkecil
+nilai.sort(reverse=False)
+print(nilai)
