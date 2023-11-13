c8e1c9f5398590db5887a542f193d79acf32ea31
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-11-13 17:47:27 +0800

    Day 36. Membuat tuple

---
+++ day36.py
@@ -0,0 +1,9 @@
+a = ("ica")
+b = "a","b","c"
+c = "majene",
+
+print("variabel a: ",a ," ,beritipe data: ",type(a))
+print("variabel a: ",b ," ,beritipe data: ",type(b))
+print("variabel a: ",c ," ,beritipe data: ",type(c))
+
+
