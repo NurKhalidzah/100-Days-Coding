ee47fe6a3573e3c7be738da22627200a3717ce75
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-11-14 17:41:15 +0800

    Day 37. Operasi tipe data tuple

---
+++ day37.py
@@ -0,0 +1,13 @@
+data = ('a','b','c','d',1,2,3,4)
+#data yang paling pertama
+print(data[0])
+#data yang paling terakhir
+print(data[-1])
+
+#menggunakan range
+#data 1 sampai 4
+print(data[:4])
+#data 4 sampai 8
+print(data[4:8])
+#melihat jumlah nilai tuple
+print(len(data))
