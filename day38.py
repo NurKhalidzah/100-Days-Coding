2ce4bde453bcd8c757d1d30d54d9837b52007978
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-11-15 20:20:48 +0800

    Day 38. Dictionary

---
+++ day38.py
@@ -0,0 +1,10 @@
+#mengakses item pada dictionary
+biodata = dict(nama="ica", umur=20, pekerjaan="Tidur dan makan")
+print(biodata["nama"])
+print(biodata["pekerjaan"])
+
+#mengakses semua key dan value dictionary
+semua_kunci = biodata.keys()
+semua_nilai = biodata.values()
+print(semua_kunci)
+print(semua_nilai)
