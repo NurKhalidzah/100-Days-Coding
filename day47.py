82448a0825daa9cd4600f4aa9b530c5afb4e0849
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-11-24 20:12:24 +0700

    Day 47. Array

---
+++ day47.py
@@ -0,0 +1,10 @@
+#menggunakan fungsi split()
+a = "nur khalidzah"
+x = a.split()
+print(x)
+
+print('************************')
+#fungsi reversed untuk membalikkan elemen 
+a = [1,2,3,4,5,6,7,8,9,10]
+for i in reversed(a):
+	print(i)
