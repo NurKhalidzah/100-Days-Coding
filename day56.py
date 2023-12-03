e3443811c44c20887fd9f083cdd6e46df8fceed6
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-12-03 20:49:38 +0800

    Day 56. Def

---
+++ day56.py
@@ -0,0 +1,13 @@
+def angka(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+a = int(input("Masukkan bilangan pertama: "))
+b = int(input("Masukkan bilangan kedua: "))
+c = int(input("Masukkan bilangan ketiga: "))
+
+print("Bilangan terbesar adalah:",angka(a, b, c))
