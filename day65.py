cd9658473745dbf4a53804d84f1696ee0ae5f767
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-12-12 20:23:51 +0800

    Day 65. If Else

---
+++ day65.py
@@ -0,0 +1,9 @@
+suka_bubur_ayam = input("Apakah Anda suka bubur ayam? (Ya/Tidak): ")
+if suka_bubur_ayam == "Ya":
+    suka_bubur_ayam_diaduk = input("Apakah Anda suka bubur ayam yang diaduk? (Ya/Tidak): ")
+    if suka_bubur_ayam_diaduk == "Tidak":
+        print("Anda ternyata manusia beradab")
+    else:
+        print("Beuh, kalau diaduk buburnya jadi mirip *? *?!")
+else:
+    print("Cobain lagi deh kapan-kapan, sehat dan bergizi, siapa tahu jadi suka")
