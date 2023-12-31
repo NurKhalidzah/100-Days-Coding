8cf6915ed6c455bbebce965b94fd90afc15ad9b1
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2023-12-31 20:14:50 +0700

    Day 84. Def

---
+++ day84.py
@@ -0,0 +1,17 @@
+def penjumlahan(a, b):
+  return a + b
+def pengurangan(a, b):
+  return a - b
+def perkalian(a, b):
+  return a * b
+def pembagian(a, b):
+  return a / b
+def sisa_bagi(a, b):
+  return a % b
+a = int(input("angka 1: "))
+b = int(input("angka 2: "))
+print("Penjumlahan:", penjumlahan(a, b))
+print("Pengurangan:", pengurangan(a, b))
+print("Perkalian:", perkalian(a, b))
+print("Pembagian:", pembagian(a, b))
+print("Sisa Bagi:", sisa_bagi(a, b))
