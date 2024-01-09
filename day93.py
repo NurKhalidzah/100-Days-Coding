9fc7aeb7dbc19337c4212d9af1d5c0f3b6cd2cf1
NurKhalidzah <99627001+NurKhalidzah@users.noreply.github.com>
2024-01-09 18:25:42 +0700

    Day 93.

---
+++ day93.py
@@ -0,0 +1,12 @@
+
+a =  int(input('Masukkan nilai a: '))
+b =  int(input('Masukkan nilai b: '))
+c =  int(input('Masukkan nilai c: '))
+
+
+if a > b and a > c:
+  print('Nilai A yang terbesar')
+elif b > a and b > c:
+  print('Nilai B yang terbesar')
+else:
+  print('Nilai C yang terbesar')
